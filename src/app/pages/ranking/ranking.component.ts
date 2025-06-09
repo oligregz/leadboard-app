576621a3074158740a925d37f2a4e6080eda0b33
oligregz <gregorysak90@gmail.com>
2025-06-09 18:57:18 -0300

    feat: add button for list users testing

--- src/app/pages/ranking/ranking.component.ts
+++ src/app/pages/ranking/ranking.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { UserService } from '@core/services/user.service';
 
 @Component({
   selector: 'app-ranking',
+  standalone: true,
   imports: [],
   templateUrl: './ranking.component.html',
   styleUrl: './ranking.component.scss',
 })
-export class RankingComponent {}
+export class RankingComponent {
+  private readonly userService = inject(UserService);
+
+  listUsers(): void {
+    this.userService.list().subscribe({
+      next: (users) => {
+        console.log(users);
+      },
+    });
+  }
+}
