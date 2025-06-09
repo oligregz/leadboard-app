8db4f95ae4f3953916a33f83ae992e74ebd24036
oligregz <gregorysak90@gmail.com>
2025-06-09 14:04:25 -0300

    style: fix lint errors

--- src/app/pages/utils/manage-local-storage.util.ts
+++ src/app/pages/utils/manage-local-storage.util.ts
@@ -1,10 +1,17 @@
-export function getLocalStorageKeyValuye(key: string): String | null {
-  if (!key) console.error('Parâmetro inválido');
+const localStorage = globalThis.localStorage;
+
+export function getLocalStorageKeyValuye(key: string): string | null {
+  if (!key) {
+    console.error('Parâmetro inválido');
+  }
   const keyValue = localStorage.getItem(key);
+
   return keyValue;
 }
 
 export function setLocalStorageKeyValue(key: string, value: string): void {
-  if (!key || !value) console.error('Parâmetros inválidos');
+  if (!key || !value) {
+    console.error('Parâmetros inválidos');
+  }
   localStorage.setItem(key, value);
 }
